692a8c93782fc45638e8c722a2b101068f288ac3
JoseliasPonton <ponton.jose30@gmail.com>
2022-05-31 18:35:10 -0500

    actualizacion backend

---
+++ src/keys.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // database credentials desarrollo
+  database: {
+    // host: 'ec2-54-204-56-171.compute-1.amazonaws.com',
+    // user:'yhmasbudpbhegc',
+    // password:'b616d57b139ccb8422e4e2cbbc9e004afe740714130084dc43d8db6806b7ead9',
+    // database:'d1tr0tfp1n847n',
+     
+    host: 'ec2-54-204-56-171.compute-1.amazonaws.com',
+    user:'yhmasbudpbhegc',
+    password:'b616d57b139ccb8422e4e2cbbc9e004afe740714130084dc43d8db6806b7ead9',
+    database:'d1tr0tfp1n847n', 
+    port:'5432',
+    ssl: { rejectUnauthorized: false }
+  },
+  SECRET : 'CLAVE ULTRA SECRETA'
+};
